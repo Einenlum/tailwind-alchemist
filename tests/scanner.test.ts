3cdfd9ac752176957e00943c44705d8f4620e654
Einenlum <contact@einenlum.com>
2025-01-30 13:51:43 +0100

    Add test for scanning multiple files

--- tests/scanner.test.ts
+++ tests/scanner.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { scanFile } from '../src/scanner';
+import { scanColors, scanFile } from '../src/scanner';
 import { FULL_RANGE_REGEX } from '../src/regexes';
 
 test('it finds all colors in file 1', async () => {
@@ -112,4 +112,206 @@ test('it finds all colors in file 2', async () => {
   expect(occurrences).toEqual(
     expected.map((occurrence) => ({ file: filePath, ...occurrence })),
   );
+});
+
+test('it finds all colors for all files', async () => {
+  const glob = 'tests/fixtures/scanner/**';
+
+  const file1 = 'tests/fixtures/scanner/file1.html';
+  const file2 = 'tests/fixtures/scanner/file2.html';
+
+  const colorMap = await scanColors([glob]);
+
+  const expected = {
+    'gray-50': [
+      { file: file1, line: 12, match: 'bg-gray-50', color: 'gray-50' },
+    ],
+    'gray-800': [
+      { file: file1, line: 12, match: 'text-gray-800', color: 'gray-800' },
+      { file: file1, line: 63, match: 'text-gray-800', color: 'gray-800' },
+      { file: file1, line: 151, match: 'text-gray-800', color: 'gray-800' },
+      { file: file1, line: 174, match: 'text-gray-800', color: 'gray-800' },
+      { file: file1, line: 192, match: 'text-gray-800', color: 'gray-800' },
+      { file: file1, line: 222, match: 'bg-gray-800', color: 'gray-800' },
+      { file: file2, line: 1, match: 'dark:bg-gray-800', color: 'gray-800' },
+      { file: file2, line: 9, match: 'text-gray-800', color: 'gray-800' },
+      { file: file2, line: 25, match: 'dark:bg-gray-800', color: 'gray-800' },
+      { file: file2, line: 78, match: 'text-gray-800', color: 'gray-800' },
+    ],
+    white: [
+      { file: file1, line: 14, match: 'bg-white', color: 'white' },
+      { file: file1, line: 32, match: 'text-white', color: 'white' },
+      { file: file1, line: 41, match: 'text-white', color: 'white' },
+      { file: file1, line: 52, match: 'bg-white', color: 'white' },
+      { file: file1, line: 73, match: 'bg-white', color: 'white' },
+      { file: file1, line: 85, match: 'bg-white', color: 'white' },
+      { file: file1, line: 97, match: 'bg-white', color: 'white' },
+      { file: file1, line: 109, match: 'bg-white', color: 'white' },
+      { file: file1, line: 121, match: 'bg-white', color: 'white' },
+      { file: file1, line: 133, match: 'bg-white', color: 'white' },
+      { file: file1, line: 161, match: 'bg-white', color: 'white' },
+      { file: file1, line: 180, match: 'bg-white', color: 'white' },
+      { file: file1, line: 202, match: 'text-white', color: 'white' },
+      { file: file1, line: 214, match: 'bg-white', color: 'white' },
+      { file: file2, line: 1, match: 'bg-white', color: 'white' },
+      { file: file2, line: 25, match: 'bg-white', color: 'white' },
+    ],
+    'indigo-600': [
+      { file: file1, line: 16, match: 'text-indigo-600', color: 'indigo-600' },
+      {
+        file: file1,
+        line: 22,
+        match: 'hover:text-indigo-600',
+        color: 'indigo-600',
+      },
+      {
+        file: file1,
+        line: 27,
+        match: 'hover:text-indigo-600',
+        color: 'indigo-600',
+      },
+      { file: file1, line: 32, match: 'bg-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 41, match: 'to-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 52, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 74, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 86, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 98, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 110, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 122, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 134, match: 'text-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 202, match: 'bg-indigo-600', color: 'indigo-600' },
+      { file: file1, line: 214, match: 'text-indigo-600', color: 'indigo-600' },
+    ],
+    'gray-900': [
+      { file: file1, line: 17, match: 'text-gray-900', color: 'gray-900' },
+      {
+        file: file2,
+        line: 57,
+        match: 'dark:hover:bg-gray-900',
+        color: 'gray-900',
+      },
+      {
+        file: file2,
+        line: 57,
+        match: 'dark:focus:bg-gray-900',
+        color: 'gray-900',
+      },
+    ],
+    'gray-600': [
+      { file: file1, line: 22, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 27, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 66, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 78, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 90, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 102, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 114, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 126, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 138, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 154, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 162, match: 'text-gray-600', color: 'gray-600' },
+      { file: file1, line: 181, match: 'text-gray-600', color: 'gray-600' },
+      {
+        file: file2,
+        line: 76,
+        match: 'dark:border-gray-600',
+        color: 'gray-600',
+      },
+    ],
+    'indigo-700': [
+      {
+        file: file1,
+        line: 32,
+        match: 'hover:bg-indigo-700',
+        color: 'indigo-700',
+      },
+    ],
+    'indigo-500': [
+      { file: file1, line: 41, match: 'from-indigo-500', color: 'indigo-500' },
+      { file: file1, line: 226, match: 'text-indigo-500', color: 'indigo-500' },
+    ],
+    'gray-200': [
+      { file: file1, line: 52, match: 'hover:bg-gray-200', color: 'gray-200' },
+      { file: file1, line: 214, match: 'hover:bg-gray-200', color: 'gray-200' },
+      { file: file2, line: 9, match: 'dark:text-gray-200', color: 'gray-200' },
+      { file: file2, line: 76, match: 'border-gray-200', color: 'gray-200' },
+      { file: file2, line: 78, match: 'dark:text-gray-200', color: 'gray-200' },
+    ],
+    'gray-100': [
+      { file: file1, line: 148, match: 'bg-gray-100', color: 'gray-100' },
+      { file: file2, line: 1, match: 'border-gray-100', color: 'gray-100' },
+      { file: file2, line: 57, match: 'hover:bg-gray-100', color: 'gray-100' },
+      { file: file2, line: 57, match: 'focus:bg-gray-100', color: 'gray-100' },
+    ],
+    'gray-500': [
+      { file: file1, line: 175, match: 'text-gray-500', color: 'gray-500' },
+      { file: file1, line: 193, match: 'text-gray-500', color: 'gray-500' },
+      { file: file2, line: 25, match: 'text-gray-500', color: 'gray-500' },
+      { file: file2, line: 57, match: 'dark:text-gray-500', color: 'gray-500' },
+      {
+        file: file2,
+        line: 57,
+        match: 'hover:text-gray-500',
+        color: 'gray-500',
+      },
+      {
+        file: file2,
+        line: 57,
+        match: 'focus:text-gray-500',
+        color: 'gray-500',
+      },
+      { file: file2, line: 79, match: 'text-gray-500', color: 'gray-500' },
+    ],
+    'gray-300': [
+      { file: file1, line: 222, match: 'text-gray-300', color: 'gray-300' },
+      {
+        file: file2,
+        line: 25,
+        match: 'dark:hover:text-gray-300',
+        color: 'gray-300',
+      },
+    ],
+    'indigo-400': [
+      { file: file1, line: 231, match: 'text-indigo-400', color: 'indigo-400' },
+    ],
+    'indigo-300': [
+      {
+        file: file1,
+        line: 231,
+        match: 'hover:text-indigo-300',
+        color: 'indigo-300',
+      },
+    ],
+    'gray-700': [
+      {
+        file: file2,
+        line: 1,
+        match: 'dark:border-gray-700',
+        color: 'gray-700',
+      },
+      {
+        file: file2,
+        line: 25,
+        match: 'hover:text-gray-700',
+        color: 'gray-700',
+      },
+    ],
+    'gray-400': [
+      { file: file2, line: 25, match: 'dark:text-gray-400', color: 'gray-400' },
+      { file: file2, line: 57, match: 'text-gray-400', color: 'gray-400' },
+      {
+        file: file2,
+        line: 57,
+        match: 'dark:hover:text-gray-400',
+        color: 'gray-400',
+      },
+      {
+        file: file2,
+        line: 57,
+        match: 'dark:focus:text-gray-400',
+        color: 'gray-400',
+      },
+    ],
+  };
+
+  expect(colorMap).toEqual(expected);
 });
